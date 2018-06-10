741e811be52feb976baf34194fa2ac9b1021e037
AndresSal <andres.salazar01@epn.edu.ec>
2018-06-09 21:45:05 -0500

    poner rutas hijas dentro de la ruta Inicio

--- Rutas-Angular/src/app/app.routes.ts
+++ Rutas-Angular/src/app/app.routes.ts
@@ -2,11 +2,23 @@ import {Router, Routes} from '@angular/router';
 import {RutaInicioComponent} from './misComponentes/ruta-inicio/ruta-inicio.component';
 import {RutaNoEncontradaComponent} from './misComponentes/ruta-no-encontrada/ruta-no-encontrada.component';
 import {RutaFAQComponent} from './misComponentes/ruta-faq/ruta-faq.component';
+import {RutaIniUsuarioComponent} from './misComponentes/ruta-ini-usuario/ruta-ini-usuario.component';
+import {RutaIniPermisosComponent} from './misComponentes/ruta-ini-permisos/ruta-ini-permisos.component';
 
 export const RUTAS_APP: Routes = [
   {
     path: 'inicio/hola/como/estas',
-    component: RutaInicioComponent
+    component: RutaInicioComponent,
+    children:[
+      {
+        path: 'usuario',
+        component: RutaIniUsuarioComponent
+      },
+      {
+        path: 'permisos',
+        component: RutaIniPermisosComponent
+      }
+    ]
   },
   {
     path: 'faq',
