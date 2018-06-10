383b87544e4a3107cf60fe538812a80a3045edff
AndresSal <andres.salazar01@epn.edu.ec>
2018-06-09 21:40:16 -0500

    creacion de componentes hijos inicioUsuario e inicioPermisos

--- Rutas-Angular/src/app/app.module.ts
+++ Rutas-Angular/src/app/app.module.ts
@@ -7,13 +7,17 @@ import { RutaNoEncontradaComponent } from './misComponentes/ruta-no-encontrada/r
 import {RouterModule} from '@angular/router';
 import {RUTAS_APP} from './app.routes';
 import { RutaFAQComponent } from './misComponentes/ruta-faq/ruta-faq.component';
+import { RutaIniUsuarioComponent } from './misComponentes/ruta-ini-usuario/ruta-ini-usuario.component';
+import { RutaIniPermisosComponent } from './misComponentes/ruta-ini-permisos/ruta-ini-permisos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     RutaInicioComponent,
     RutaNoEncontradaComponent,
-    RutaFAQComponent
+    RutaFAQComponent,
+    RutaIniUsuarioComponent,
+    RutaIniPermisosComponent
   ],
   imports: [
     BrowserModule,
